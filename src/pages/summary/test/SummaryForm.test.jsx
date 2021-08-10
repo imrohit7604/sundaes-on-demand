01de13b1ea51f9cab304878c5eb3641c1a36cd01
Rohit <imrohit7604@gmail.com>
2021-08-10 18:03:03 +0530

    Mock server Added

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
 import SummaryForm from '../SummaryForm';
-const { render, screen, fireEvent } = require("@testing-library/react")
+import userEvent from "@testing-library/user-event"
+const { render, screen, waitForElementToBeRemoved } = require("@testing-library/react")
 
 test("inital condition", () => {
     render(<SummaryForm />)
@@ -11,15 +12,41 @@ test("inital condition", () => {
 })
 
 test("checkbox enables button on first click and disable on select click", () => {
-    render(<SummaryForm/>)
+    render(<SummaryForm />)
     const checkbox = screen.getByRole("checkbox", { name: /terms and condition/i })
     const confirmButton = screen.getByRole("button", { name: /confirm order/i })
 
-    fireEvent.click(checkbox)
+    userEvent.click(checkbox)
     expect(checkbox).toBeChecked()
     expect(confirmButton).toBeEnabled()
 
-    fireEvent.click(checkbox)
+    userEvent.click(checkbox)
     expect(checkbox).not.toBeChecked()
     expect(confirmButton).toBeDisabled()
+})
+
+
+test("popover responds to hover", async () => {
+    render(<SummaryForm />)
+    //popover starts at hidden
+    const nullPopOver = screen.queryByText(/No ice cream will actually be delivered/i)
+    expect(nullPopOver).not.toBeInTheDocument()
+
+
+    //popover appears upon mouseover of checkbox label
+    const termsAndCondition = screen.getByText(/terms and conditions/i)
+    userEvent.hover(termsAndCondition)
+
+
+    const popOver = screen.getByText(/No ice cream will actually be delivered/i)
+    expect(popOver).toBeInTheDocument()
+
+    //popover disappears when we mouse out
+    userEvent.unhover(termsAndCondition)
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/No ice cream will actually be delivered/i))
+
+
+
+
 })
